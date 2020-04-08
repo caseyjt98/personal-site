f3a6a2313993494c6fd7cf60a020399afbe44650
Casey Taylor <caseyjt98@gmail.com>
2020-04-07 21:04:35 -0700

    implement rendering of basic page views

---
+++ src/HomePage/HomePage.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component<any> {
+    render() {
+        return (
+            <div>
+                <header> Home Page </header>
+                <div>
+                    I'm Casey Taylor, and I'm a full stack developer. Welcome to my website.
+                </div>
+            </div>
+
+        )
+    }
+}
+
+export default HomePage;
