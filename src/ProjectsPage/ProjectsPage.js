36b43d857de0b5202ec77b0d6b0808780842887e
Casey Taylor <caseyjt98@gmail.com>
2020-04-08 20:07:53 -0700

    fixing styles

--- src/ProjectsPage/ProjectsPage.js
+++ src/ProjectsPage/ProjectsPage.js
@@ -13,16 +13,19 @@ class ProjectPage extends React.Component {
                 <div className="xtra-padding" />
 
 
-                <div className="xtra-padding">
-                    <h1> Homebru </h1>
+                <div className="xtra-padding-sm">
+                    <h1 className="project-heading"> Homebru </h1>
                     <p> A mobile application built on Angular Iconic Native and serving as a method of communication for UCLA students seeking apartment subletters or subleasers during the academic school year.</p>
                     <a href="https://github.com/caseyjt98/subletting-api"> Find me on Github! (Back end) </a>
                     <br />
                     <a href="https://github.com/caseyjt98/Homebru-Frontend"> Find me on Github! (Front end) </a>
                 </div>
 
-                <div>
-                    <h1> Aphrodite </h1>
+                <hr />
+                <div className="xtra-padding-sm" />
+
+                <div className="xtra-padding-sm">
+                    <h1 className="project-heading"> Aphrodite </h1>
                     <p> A web application built using React and intended to serve as an interactive class and activity planner for UCLA students, based on their interests and major. </p>
                     <a href="https://github.com/ucladevx/Aphrodite"> Find me on Github! </a>
                 </div>
--- src/WorkPage/WorkPage.js
+++ src/WorkPage/WorkPage.js
@@ -5,75 +5,27 @@ import './WorkPage.css'
 
 class WorkPage extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedView: 'home'
-        }
-    }
-
-    // displayStem = () => {
-    //     this.setState({ selectedView: 'stem' })
-    // }
-
-    // displayFactual = () => {
-    //     console.log("display project page called")
-    //     this.setState({ selectedView: 'factual' })
-    // }
-
-    // displayUliza = () => {
-    //     this.setState({ selectedView: 'uliza' })
-    // }
-
     render() {
-
-        let currentView
-
-        switch (this.state.selectedView) {
-            case 'home':
-                currentView = (
-                    <div className="container" >
-                        <div className="typewriter">
-                            Explore my work experience here.
-                    </div>
-                        <Row className="row-one">
-                            <Col lg={{ span: 6, offset: 5 }}>
-                                <button className="work-button" id="stem"> Stem Disintermedia </button>
-                            </Col>
-                            <Col lg={{ span: 6, offset: 2 }}>
-                                <button className="work-button" id="factual"> Factual </button>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col lg={{ span: 6, offset: 9 }}>
-                                <button className="work-button" id="uliza"> Uliza </button>
-                            </Col>
-                        </Row>
-                    </div >
-                )
-                break
-            case 'stem':
-                currentView = (
-                    <div>stem</div>
-                )
-                break
-            case 'factual':
-                currentView = (
-                    <div>factual</div>
-                )
-                break
-            case 'uliza':
-                currentView = (
-                    <div>uliza</div>
-                )
-                break
-            default:
-                currentView = <div>No Content</div>
-        }
-
         return (
-            <div className="container">
-                {currentView}
+            <div >
+                <div className="typewriter" id="work-container">
+                    Explore my work experience here.
+                </div>
+                <div className="xtra-padding" />
+                <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
+                {/* <Row className="row-one">
+                    <Col lg={{ span: 6, offset: 5 }}>
+                        <button className="work-button" id="stem"> Stem Disintermedia </button>
+                    </Col>
+                    <Col lg={{ span: 6, offset: 2 }}>
+                        <button className="work-button" id="factual"> Factual </button>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col lg={{ span: 6, offset: 9 }}>
+                        <button className="work-button" id="uliza"> Uliza </button>
+                    </Col>
+                </Row> */}
             </div >
 
         )
